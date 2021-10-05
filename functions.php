4c5053c4c004b584f491014dea1e96e8e05f1b7c
Ross Wintle <rosswintle@users.noreply.github.com>
2021-10-05 13:45:46 +0100

    Add one_time parameter to the dbi_get_auto_login_url() function

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! function_exists( 'dbi_get_auto_login_url' ) ) {
-	function dbi_get_auto_login_url( $url, $user_id, $args = array(), $expires_in = null ) {
+	function dbi_get_auto_login_url( $url, $user_id, $args = array(), $expires_in = null, $one_time = false ) {
 		$autologin = \DeliciousBrains\WPAutoLogin\AutoLogin::instance();
 
-		return $autologin->create_url( $url, $user_id, $args, $expires_in );
+		return $autologin->create_url( $url, $user_id, $args, $expires_in, $one_time );
 	}
 }
